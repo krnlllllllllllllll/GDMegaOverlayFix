f83d70343dd2d447ed25c259e3f1d147ff922eeb
krnlllllllllllllll <jworley419@gmail.com>
2026-06-28 21:24:01 -0400

    Update SimpleHacks.cpp

--- src/Hacks/SimpleHacks.cpp
+++ src/Hacks/SimpleHacks.cpp
@@ -1,62 +1,63 @@
 #include "Common.h"
 #include "Settings.hpp"
 
+#include <Geode/Geode.hpp>
 #include <Geode/modify/HardStreak.hpp>
 #include <Geode/modify/PlayLayer.hpp>
 
 using namespace geode::prelude;
 
-class $modify(HardStreak)
-{
-	void updateStroke(float dt)
-	{
-		if (Settings::get<bool>("player/trail/enabled", false))
-		{
-			float trailSize = Settings::get<float>("player/trail/size", 1.f);
-			MBO(float, this, 0x168) = trailSize;
+class $modify(HardStreak) {
+    void updateStroke(float dt) {
+        if (Settings::get<bool>("player/trail/enabled", false)) {
+            float trailSize = Settings::get<float>("player/trail/size", 1.f);
+            MBO(float, this, 0x168) = trailSize;
 
-			if (Settings::get<bool>("player/trail/color/enabled", false))
-			{
-				float trailColor[3]{
-					Settings::get<float>("player/trail/color/r", 1.f),
-					Settings::get<float>("player/trail/color/g", 1.f),
-					Settings::get<float>("player/trail/color/b", 1.f)
-				};
+            if (Settings::get<bool>("player/trail/color/enabled", false)) {
+                float trailColor[3]{
+                    Settings::get<float>("player/trail/color/r", 1.f),
+                    Settings::get<float>("player/trail/color/g", 1.f),
+                    Settings::get<float>("player/trail/color/b", 1.f)
+                };
 
-				this->setColor({
-					(byte)(trailColor[0] * 255),
-					(byte)(trailColor[1] * 255),
-					(byte)(trailColor[2] * 255)}
-				);
-			}
-		}
+                this->setColor({
+                    static_cast<GLubyte>(trailColor[0] * 255.f),
+                    static_cast<GLubyte>(trailColor[1] * 255.f),
+                    static_cast<GLubyte>(trailColor[2] * 255.f)
+                });
+            }
+        }
 
-		HardStreak::updateStroke(dt);
-	}
+        HardStreak::updateStroke(dt);
+    }
 };
 
-class $modify(PlayLayer)
-{
-	geode::Patch* AnticheatPatch;
+class $modify(PlayLayer) {
+public:
+    struct Fields {
+        Ref<geode::Patch> anticheatPatch;
+    };
 
-	void resetLevel()
-	{
-		bool instantComplete = Settings::get<bool>("level/instant_complete", false);
+    void resetLevel() {
+        bool instantComplete = Settings::get<bool>("level/instant_complete", false);
 
-		if (instantComplete)
-		{
-			// skips the whole if check that checks for time in level and a bunch of other stuff
-			m_fields->AnticheatPatch = Mod::get()->patch(reinterpret_cast<void*>(base::get() + 0x2DDC03), { 0xEB, 0x53 }).unwrap();
-			m_fields->AnticheatPatch->enable();
-		}
+        if (instantComplete) {
+            auto res = Mod::get()->patch(
+                reinterpret_cast<void*>(base::get() + 0x2DDC03),
+                { 0xEB, 0x53 }
+            );
 
-		PlayLayer::resetLevel();
+            if (res.isOk()) {
+                m_fields->anticheatPatch = res.unwrap();
+                m_fields->anticheatPatch->enable();
+            }
+        }
 
-		if (instantComplete)
-		{
-			this->playPlatformerEndAnimationToPos({ .0f, 105.f }, true);
+        PlayLayer::resetLevel();
 
-			m_fields->AnticheatPatch->disable();
-		}
-	}
+        if (instantComplete && m_fields->anticheatPatch) {
+            this->playPlatformerEndAnimationToPos({ 0.f, 105.f }, true);
+            m_fields->anticheatPatch->disable();
+        }
+    }
 };
