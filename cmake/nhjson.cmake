647235e1affac94a9a3ccd098dbd5091c01f20a9
krnlllllllllllllll <jworley419@gmail.com>
2026-06-28 21:11:55 -0400

    Update nhjson.cmake

--- cmake/nhjson.cmake
+++ cmake/nhjson.cmake
@@ -1,13 +1,13 @@
+# NHJSON for Geode 5.7.1
+
 include(FetchContent)
 
-set(JSON_MultipleHeaders OFF)
-
-# deprecated, move to nlohmann-json releases
 FetchContent_Declare(
     nhjson
-    GIT_REPOSITORY https://github.com/ArthurSonzogni/nlohmann_json_cmake_fetchcontent.git
-    GIT_TAG        326308d7512d2168ae00199aec4dd0f714526e89
-    GIT_PROGRESS TRUE
+    GIT_REPOSITORY https://github.com/SpaghettDev/nhjson.git
+    GIT_TAG main
 )
-message("Fetching nlohmann-json")
+
 FetchContent_MakeAvailable(nhjson)
+
+target_link_libraries(${PROJECT_NAME} PRIVATE nhjson)
