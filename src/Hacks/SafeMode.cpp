c09333e599e97ad92135be06f771bd44d6769cb0
krnlllllllllllllll <jworley419@gmail.com>
2026-06-28 22:11:39 -0400

    Update SafeMode.cpp

--- src/Hacks/SafeMode.cpp
+++ src/Hacks/SafeMode.cpp
@@ -1,95 +1,31 @@
 #include "SafeMode.h"
 
-#include <Geode/modify/EndLevelLayer.hpp>
-
-#include <Geode/Utils.hpp>
-#include <Geode/platform/windows.hpp>
-
 #include <Geode/Geode.hpp>
+#include <Geode/modify/EndLevelLayer.hpp>
+#include <Geode/binding/FMODAudioEngine.hpp>
 
-#include "../util.hpp"
 #include "../Settings.hpp"
-
-#include "Common.h"
+#include "../Common.h"
 
 using namespace geode::prelude;
 using namespace SafeMode;
 
-class $modify(EndLevelLayer)
-{
-	void customSetup()
-	{
-		EndLevelLayer::customSetup();
+class $modify(EndLevelLayer_SafeMode, EndLevelLayer) {
+public:
+    void customSetup() {
+        EndLevelLayer::customSetup();
 
-		if (
-			!Settings::get<bool>("level/safe_mode/enabled") ||
-			!Settings::get<bool>("level/safe_mode/endscreen_enabled")
-			) return;
+        if (
+            !Settings::get<bool>("level/safe_mode/enabled") ||
+            !Settings::get<bool>("level/safe_mode/endscreen_enabled")
+        ) return;
 
-		auto layer = reinterpret_cast<CCLayer*>(this->getChildren()->objectAtIndex(0));
-		CCLabelBMFont* endScreenMessageLabel = nullptr;
-		TextArea* endScreenTextArea = nullptr;
+        // EndLevelLayer children:
+        // [0] = main content layer
+        auto layer = static_cast<CCLayer*>(this->getChildren()->objectAtIndex(0));
 
+        CCLabelBMFont* endScreenMessageLabel = nullptr;
+        TextArea* endScreenTextArea = nullptr;
 
-		CCObject* object;
-		CCARRAY_FOREACH(layer->getChildren(), object)
-		{
-			if (auto textArea = dynamic_cast<TextArea*>(object); textArea)
-				endScreenTextArea = textArea;
-			else if (
-				auto messageLabel = dynamic_cast<CCLabelBMFont*>(object);
-				messageLabel
-			) {
-				std::string labelText = messageLabel->getString();
-
-				if (
-					!labelText.starts_with("Attempts: ") && !labelText.starts_with("Jumps: ") &&
-					!labelText.starts_with("Time: ") && !labelText.starts_with("Points: ")
-					)
-					endScreenMessageLabel = messageLabel;
-			}
-		}
-
-		// TODO: Create a CCLabelBMFont if both of these are false
-
-		if (endScreenTextArea)
-			endScreenTextArea->setString("- Safe Mode -");
-
-		if (endScreenMessageLabel)
-			endScreenMessageLabel->setString("- Safe Mode -");
-	}
-};
-
-void SafeMode::updateAuto()
-{
-	if (Settings::get<bool>("level/safe_mode/auto", false))
-		Mod::get()->setSavedValue<bool>("level/safe_mode/enabled", Common::isCheating);
-
-	updateState();
-}
-
-void SafeMode::updateState()
-{
-	for (auto& patch : patches)
-	{
-		if (Settings::get<bool>("level/safe_mode/enabled"))
-		{
-			if (!patch->isEnabled())
-				patch->enable();
-		}
-		else
-		{
-			if (patch->isEnabled())
-				patch->disable();
-		}
-	}
-}
-
-$execute
-{
-	for (std::size_t i = 0; i < PATCHES_SIZE; i++)
-	{
-		patches[i] = Mod::get()->patch(reinterpret_cast<void*>(base::get() + std::get<0>(opcodes[i])), std::get<1>(opcodes[i])).unwrap();
-		patches[i]->disable();
-	}
-}
+        CCObject* obj;
+        CC
