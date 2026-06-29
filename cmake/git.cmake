919b6aa6d8de58426f9149d410718b3ff4b88217
krnlllllllllllllll <jworley419@gmail.com>
2026-06-28 21:10:06 -0400

    Update git.cmake

--- cmake/git.cmake
+++ cmake/git.cmake
@@ -1,4 +1,16 @@
-find_package(Git)
-if(Git_FOUND)
-    message("Git found: ${GIT_EXECUTABLE}")
+# Git metadata for Geode 5.7.1 builds
+
+find_package(Git QUIET)
+
+if(GIT_FOUND)
+    execute_process(
+        COMMAND ${GIT_EXECUTABLE} rev-parse --short HEAD
+        WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+        OUTPUT_VARIABLE GIT_COMMIT
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+else()
+    set(GIT_COMMIT "unknown")
 endif()
+
+set(GEODE_GIT_COMMIT "${GIT_COMMIT}" PARENT_SCOPE)
